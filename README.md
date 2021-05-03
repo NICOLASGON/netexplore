fe80a255e6f9ae0fa26a174830d1d4578ec926d7
Nicolas Gonzalez <ngonzalez@operametrix.fr>
2021-05-03 22:50:38 +0200

    Fix issue with VM bootscript

--- README.md
+++ README.md
@@ -11,8 +11,7 @@ This project is mainly inspired by :
 ### Packages
 
 Installation of some packages on your host system : \
-`sudo apt install lxc bridge-utils python3-lxc` \
-`sudo pip3 install graphviz` 
+`sudo apt install lxc bridge-utils python3-lxc python3-graphviz` \
 
 ### Networking
 
--- vm/debian_buster.sh
+++ vm/debian_buster.sh
@@ -3,8 +3,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install -y git lxc bridge-utils python3-lxc
-sudo pip3 install graphviz
+sudo apt install -y git lxc bridge-utils python3-lxc python3-graphviz
 
 cd ~
 git clone https://github.com/NICOLASGON/netexplore.git
@@ -14,9 +13,9 @@ USE_LXC_BRIDGE="true"
 EOL
 
 cat > /etc/lxc/default.conf << EOL
-lxc.net[0].type = veth
-lxc.net[0].link = lxcbr0
-lxc.net[0].flags = up
+lxc.net.0.type = veth
+lxc.net.0.link = lxcbr0
+lxc.net.0.flags = up
 EOL
 
 sudo service lxc-net start
